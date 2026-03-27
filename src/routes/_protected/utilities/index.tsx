16e2b70b0e474144d478b70ed684b1975a8d653b
Abdul Rehman-z <yousafbhaikhan10@gmail.com>
2026-03-27 18:44:37 +0500

    feat: Refactor finance and

--- src/routes/_protected/utilities/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_protected/utilities/")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/utilities/dashboard"!</div>;
-}
